e5df7929bc346fb9c2183140cd9ecc3ab8c87141
gbriyad <gbriyad@gmail.com>
2019-10-23 01:31:03 +0600

    update ingredient information when any purchase is made

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
+  has_many :purchases, dependent: :restrict_with_error
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :feed_formulation_quantity, presence: true
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,28 @@
+# warnings
+# editing/deleting old purchases should be restricted
+# if any feed has been created after that particular purchase
 class Purchase < ApplicationRecord
   belongs_to :ingredient
   validates :quantity, :price_per_unit, :date, presence: true
+
+  after_create do
+    update_ingredient_information price_per_unit, quantity
+  end
+
+  after_update do
+    change = saved_change_to_attribute :quantity
+    update_ingredient_information price_per_unit, change[1] - change[0]
+  end
+
+  after_destroy do
+    update_ingredient_information nil, -quantity
+  end
+
+  def update_ingredient_information(price_per_unit, quantity_to_be_added)
+    if price_per_unit.present?
+      ingredient.last_purchased_price_per_unit = price_per_unit
+    end
+    ingredient.stock_quantity += quantity_to_be_added
+    ingredient.save!
+  end
 end
