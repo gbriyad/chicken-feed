8d85d483ed9985825c7ab381d3f5b40b9b89ff17
gbriyad <gbriyad@gmail.com>
2019-11-12 01:18:50 +0600

    role field added for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: %i[super_admin admin staff]
+  validates :role, presence: true
+
 end
